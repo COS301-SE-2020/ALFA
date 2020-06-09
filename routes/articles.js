94329d6cbd86791a35aa86b92a1d7a158a4c14ed
Pako Diale <44471692+pkdiale671@users.noreply.github.com>
2020-06-09 20:23:46 +0200

    code to retrieve all kb articles

--- routes/articles.js
+++ routes/articles.js
@@ -2,25 +2,7 @@ const express = require('express')
 const router = express.Router()
 const KB_Article = require('../models/kb_article')
 
-//Endpoint to save uploded file contents to db
-router.post('/', async(req,res)=>{
-    try {
-        let data = req.body
-        const article = new KB_Article({
-            
-        })
-
-        const newArticle = await article.save()
-        res.json(newArticle)
-        console.log("New Record Saved!")
-        console.log(newArticle)
-    } catch (error) {
-        res.json({message:error})
-        console.log(error)
-    }
-})
-
-//Endpoint to retrieve all logfiles
+// Endpoint to retrieve all KB articles
 router.get('/', async(res,req)=>{
     try {
         const AllArticles = await KB_Article.find()
@@ -30,3 +12,5 @@ router.get('/', async(res,req)=>{
         console.log(error)
     }
 })
+
+module.export = router
