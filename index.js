24ffb1835e6b42fc26786494f9b98021fd6133c3
lamboughs <mlambolindo6@gmail.com>
2020-07-22 02:30:42 +0200

    Fixing Heroku error, kept crashing

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ app.get('/', (req, res)=>{
     res.send("API IS Running")
 })
 //listening to incoming connections
-const PORT = process.env.PORT || 8090;
+const PORT = process.env.PORT || 2221;
 app.listen(PORT,()=>{
     console.log(`Server Running on localhost:${PORT}`)
 })
