fb98b43fb92b8bc476ad3725446cc7a0513f2296
Orifha Mbedzi <orifha@mbedzi.co.za>
2020-08-19 21:49:03 +0200

    Navbar tested, check if all links are rendered.

--- src/app/navbar/navbar.component.spec.ts
+++ src/app/navbar/navbar.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NavbarComponent } from './navbar.component';
+import {By} from '@angular/platform-browser';
 
 describe('NavbarComponent', () => {
   let component: NavbarComponent;
@@ -21,5 +22,20 @@ describe('NavbarComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('it should contain Home link', () => {
+    const links = fixture.debugElement.nativeElement.querySelectorAll('.nav-item');
+    const homeLink = links[0];
+    expect(homeLink.innerHTML).toContain('Home');
+  });
+  it('it should contain How it works link', () => {
+    const links = fixture.debugElement.nativeElement.querySelectorAll('.nav-item');
+    const homeLink = links[1];
+    expect(homeLink.innerHTML).toContain('How it Works');
+  });
+  it('it should contain Analysis History link', () => {
+    const links = fixture.debugElement.nativeElement.querySelectorAll('.nav-item');
+    const homeLink = links[2];
+    expect(homeLink.innerHTML).toContain('Analysis History');
   });
 });
--- src/app/suggestion-box/suggestion-box.component.spec.ts
+++ src/app/suggestion-box/suggestion-box.component.spec.ts
@@ -30,41 +30,41 @@ describe('SuggestionBoxComponent', () => {
   });
 
   it('should test form validify', ()=>{
-      const form = component.form
-      expect(form.valid).toBeFalsy()
+      const form = component.form;
+      expect(form.valid).toBeFalsy();
 
-      const linkInput = form.controls.link
-      linkInput.setValue('https://stackoverflow.com/questions/47511736/how-to-test-forms-with-jasmine')
+      const linkInput = form.controls.link;
+      linkInput.setValue('https://stackoverflow.com/questions/47511736/how-to-test-forms-with-jasmine');
 
-      const descriptionInput = form.controls.description
-      descriptionInput.setValue('How to test forms with Jasmine')
+      const descriptionInput = form.controls.description;
+      descriptionInput.setValue('How to test forms with Jasmine');
 
-      expect(form.valid).toBeTruthy()
-  })
+      expect(form.valid).toBeTruthy();
+  });
 
-  it('should test input validity', ()=>{
-    const linkInput = component.form.controls.link
-    const descriptionInput = component.form.controls.description
+  it('should test input validity', () => {
+    const linkInput = component.form.controls.link;
+    const descriptionInput = component.form.controls.description;
 
-    expect(linkInput.valid).toBeFalsy()
-    expect(descriptionInput.valid).toBeFalsy()
+    expect(linkInput.valid).toBeFalsy();
+    expect(descriptionInput.valid).toBeFalsy();
 
-    linkInput.setValue('https://stackoverflow.com/questions/47511736/how-to-test-forms-with-jasmine')
-    descriptionInput.setValue('How to test forms with Jasmine')
+    linkInput.setValue('https://stackoverflow.com/questions/47511736/how-to-test-forms-with-jasmine');
+    descriptionInput.setValue('How to test forms with Jasmine');
 
-    expect(linkInput.valid).toBeTruthy()
-    expect(descriptionInput.valid).toBeTruthy()
-  })
+    expect(linkInput.valid).toBeTruthy();
+    expect(descriptionInput.valid).toBeTruthy();
+  });
 
-  it('should submit suggestion', ()=>{
-    component.form.controls.link.setValue('https://stackoverflow.com/questions/47511736/how-to-test-forms-with-jasmine')
+  it('should submit suggestion', () => {
+    component.form.controls.link.setValue('https://stackoverflow.com/questions/47511736/how-to-test-forms-with-jasmine');
     component.form.controls.description.setValue('How to test forms with Jasmine')
 
-    fixture.debugElement.query(By.css('form')).triggerEventHandler('ngSubmit', null); 
+    fixture.debugElement.query(By.css('form')).triggerEventHandler('ngSubmit', null);
     fixture.detectChanges();
 
     expect(MessageComponent).toBeTruthy();
-  })
+  });
 
 
 });
