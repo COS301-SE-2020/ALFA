0a0ad703e7c3b53992a69db358214ef31eabadc9
DuncanSmallwood <duncansmallwood@yahoo.com>
2020-09-15 15:43:50 +0200

    added storage for user types

--- models/user.js
+++ models/user.js
@@ -13,6 +13,17 @@ const UserSchema = new mongoose.Schema({
 		type: String,
 		required: true
 	}
+	/*
+	should be either 'normal' or 'data_scientist' or 'admin'
+	normal: a standard user with minimal privileges
+	data_scientist: a data scientist that should be able to train the ML(not implemented, 15/09/2020)
+	admin: the only user that can create data scientists and other admins
+	*/
+	user_type:{
+		type: String,
+		required: true,
+		default: 'normal'
+	}
 });
 
 module.exports = mongoose.model('user', UserSchema);
