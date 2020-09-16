4183cf6334c6d9c2cd61696c9c3cc8166492166f
u16220073 <u16220073@tuks.co.za>
2020-09-16 23:28:28 +0200

    save

--- routes/logfiles.js
+++ routes/logfiles.js
@@ -1,40 +1,22 @@
-<<<<<<< HEAD
-=======
-//BY JABULANI SHILI
->>>>>>> 9890ac99e74b20b72adb0b444c76aadd8442f0db
 const express = require('express')
 const router = express.Router()
 const Logfile = require('../models/log_file')
 const KB_Article = require('../models/kb_article')
-<<<<<<< HEAD
 
 router.get('/retrieve', async(req, res) => {
 	try{
 		let data = req.body;
-=======
-const User = require('../model/user');
-
-router.post('/retrieve' async(req, res) => {
-	try{
-		let data = req.body;
-
->>>>>>> 9890ac99e74b20b72adb0b444c76aadd8442f0db
 		if(data.user_id == null){
 			throw '401';
 		}
 		else{
-<<<<<<< HEAD
             let isUser = await User.findByID(data.user_id).exec();
             let limit;
-=======
-			let isUser = await User.findByID(data.user_id).exec();
->>>>>>> 9890ac99e74b20b72adb0b444c76aadd8442f0db
 			if(isUser == null){
 				throw '401';
 			}
 			else{
 				if(data.num_retrieve == null){
-<<<<<<< HEAD
 					limit = 25;
 				}
 				else{
@@ -45,37 +27,15 @@ router.post('/retrieve' async(req, res) => {
 					.exec();
 
 				res.status(200).json(logfiles);
-=======
-					let limit = 25;
-				}
-				else{
-					let limit = data.num_retrieve;
-				}
-				let logfiles = await Logfile.find(user_id : data.user_id)
-					.limit(limit)
-					.exec();
-
-				res.status(200)
-					.json(logfiles);
->>>>>>> 9890ac99e74b20b72adb0b444c76aadd8442f0db
 			}
 		}
 	}
 	catch(error){
 		if(error == '401'){
-<<<<<<< HEAD
 			res.status(401).json({message : 'unauthorised attempt to retrieve logfiles'});
 		}
 		else{
 			res.status(500).json({message : error.message});
-=======
-			res.status(401)
-				.json(message : 'unauthorised attempt to retrieve logfiles');
-		}
-		else{
-			res.status(500)
-        		.json(message : error);
->>>>>>> 9890ac99e74b20b72adb0b444c76aadd8442f0db
         }
 	}
 });
@@ -103,7 +63,6 @@ router.post('/upload', async(req,res)=>{
         })
 
         const newFile = await file.save()
-<<<<<<< HEAD
         if(newFile){
             /**
              * Calls to the AI API should be done here
@@ -160,8 +119,6 @@ router.post('/', async(req,res)=>{
         })
 
         const newFile = await file.save()
-=======
->>>>>>> 9890ac99e74b20b72adb0b444c76aadd8442f0db
         // res.json(newFile)
         if(newFile){
             /**
