0a38b02d5540d4547a082fcf33e624fd199ed23b
Orifha Mbedzi <orifha@mbedzi.co.za>
2020-09-17 14:39:23 +0200

    End To End Testing

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,5 +1,6 @@
 import { AppPage } from './app.po';
-import { browser, logging } from 'protractor';
+import { browser, logging, by, element } from 'protractor';
+import { protractor } from 'protractor/built/ptor';
 
 describe('workspace-project App', () => {
   let page: AppPage;
@@ -10,9 +11,46 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('alfa-ui app is running!');
+    expect(page.getTitleText()).toEqual('ALFA: Automated Log File Analyzer');
   });
+  it('should initially have solve error button disabled', () => {
+    const button = element(by.buttonText('Solve error'));
+    expect(button.isEnabled()).toEqual(false);
+  });
+  it('should have errors input field', () => {
+    const field = element(by.id('error-inp'));
+    expect(field.isDisplayed()).toEqual(true);
+  });
+  it('should take errors as input on textarea', () => {
+    const field = element(by.id('error-inp'));
+    field.click();
+    field.clear();
+    field.sendKeys('[Mon Feb 12 13:35:40.960021 2018] [autoindex:error] [pid 25057] [client 127.0.0.1:57668] AH01276: Cannot serve directory /home/cs/students/u16220073/: No matching DirectoryIndex (index.html,index.html.var,index.php,index.phtml) found, and server-generated directory index forbidden by Options directive    ');
+    expect(field.getAttribute('value')).toEqual('[Mon Feb 12 13:35:40.960021 2018] [autoindex:error] [pid 25057] [client 127.0.0.1:57668] AH01276: Cannot serve directory /home/cs/students/u16220073/: No matching DirectoryIndex (index.html,index.html.var,index.php,index.phtml) found, and server-generated directory index forbidden by Options directive    ');
+  });
+  it('Solve errors should be clickable after adding input to field', () => {
+    const field = element(by.id('error-inp'));
+    field.click();
+    field.clear();
+    field.sendKeys('[Mon Feb 12 13:35:40.960021 2018] [autoindex:error] [pid 25057] [client 127.0.0.1:57668] AH01276: Cannot serve directory /home/cs/students/u16220073/: No matching DirectoryIndex (index.html,index.html.var,index.php,index.phtml) found, and server-generated directory index forbidden by Options directive    ');
+    const button = element(by.buttonText('Solve error'));
+    expect(button.isEnabled()).toEqual(true);
+  });
+  it('should click button to send logfile', () => {
+    const field = element(by.id('error-inp'));
+    field.click();
+    field.clear();
+    field.sendKeys('[Mon Feb 12 13:35:40.960021 2018] [autoindex:error] [pid 25057] [client 127.0.0.1:57668] AH01276: Cannot serve directory /home/cs/students/u16220073/: No matching DirectoryIndex (index.html,index.html.var,index.php,index.phtml) found, and server-generated directory index forbidden by Options directive    ');
+    const button = element(by.buttonText('Solve error'));
+    button.click();
+    const elem = element(by.id('analysis-result'));
+    expect(browser.wait(protractor.ExpectedConditions.presenceOf(elem)));
 
+  });
+  it('should redirect user to redirect', () => {
+    const elem = element(by.buttonText('View solution'));
+    expect(browser.wait(protractor.ExpectedConditions.presenceOf(elem)));
+  })
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
     const logs = await browser.manage().logs().get(logging.Type.BROWSER);
