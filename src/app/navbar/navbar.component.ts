5ec3fba04ad34f88685f324442fe3db1950e6cb7
lamboughs <mlambolindo6@gmail.com>
2020-10-02 23:17:18 +0200

    Profile page plugged to third party account management

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 import { UserManService } from '../user-man.service';
 import { AuthService } from '../auth.service';
+import { MessageService } from '../message.service';
+
 @Component({
     selector: 'app-navbar',
     templateUrl: './navbar.component.html',
@@ -11,7 +13,7 @@ export class NavbarComponent implements OnInit {
     isSignIn: boolean;
     isLoggedIn: boolean;
 
-    constructor(private router: Router, private userService: UserManService, public auth: AuthService) {
+    constructor(private router: Router, private userService: UserManService, public auth: AuthService, private messageService: MessageService) {
         this.router.events.forEach(evt => {
             if (evt instanceof NavigationEnd) {
                 this.isSignIn = (evt.url === '/signin');
@@ -24,5 +26,6 @@ export class NavbarComponent implements OnInit {
     }
     signout() {
         this.userService.signout();
+        this.messageService.notify(`You have signed out succesfully`);
     }
 }
